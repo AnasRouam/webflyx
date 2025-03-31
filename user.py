659a82628a9701a58e19fc1167d51bdc85c54d82
AnasRouam <114496477+AnasRouam@users.noreply.github.com>
2025-03-31 21:33:26 +0000

    Implemented init, repr and str methods for User Class

--- user.py
+++ user.py
@@ -1,9 +1,11 @@
 class User:
-    def __init__(self):
-        pass
+    def __init__(self, name, last_name, age):
+        self.name = name
+        self.last_name = last_name
+        self.age = age
 
     def __repr__(self):
-        pass
-    
+        print(f'User({self.name}, {self.last_name}, {self.age})')
+
     def __str__(self):
-        pass
+        print(f'This User is called {self.name} {self.last_name}. He/She is {self.age}')
