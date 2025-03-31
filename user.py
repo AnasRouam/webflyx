9a176d7ebad733241ce1e92f1d724f53b4d5c4fc
AnasRouam <114496477+AnasRouam@users.noreply.github.com>
2025-03-31 22:12:03 +0000

    Fixed repr and str in User class. Added app.py with implementation of the first library client

--- user.py
+++ user.py
@@ -1,12 +1,13 @@
 class User:
-    def __init__(self, name, last_name, age, email):
+    def __init__(self, name, last_name, age, email, rented_books_ids):
         self.name = name
         self.last_name = last_name
         self.age = age
         self.email = email
+        self.rented_books_ids = rented_books_ids
 
     def __repr__(self):
-        print(f'User("{self.name}", "{self.last_name}", "{self.age}", "{self.email}")')
+        return f'User("{self.name}", "{self.last_name}", "{self.age}", "{self.email}")'
 
     def __str__(self):
-        print(f'This User is called {self.name} {self.last_name}. He/She is {self.age}. You can reach him via email at {self.email}')
+        return f'This User is called {self.name} {self.last_name}. He/She is {self.age}. You can reach him via email at {self.email}'
