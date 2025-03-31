3d44580fa215826bea58f4e4b1767720054d9406
AnasRouam <114496477+AnasRouam@users.noreply.github.com>
2025-03-31 21:48:46 +0000

    Added email to user class

--- user.py
+++ user.py
@@ -1,11 +1,12 @@
 class User:
-    def __init__(self, name, last_name, age):
+    def __init__(self, name, last_name, age, email):
         self.name = name
         self.last_name = last_name
         self.age = age
+        self.email = email
 
     def __repr__(self):
-        print(f'User({self.name}, {self.last_name}, {self.age})')
+        print(f'User("{self.name}", "{self.last_name}", "{self.age}", "{self.email}")')
 
     def __str__(self):
-        print(f'This User is called {self.name} {self.last_name}. He/She is {self.age}')
+        print(f'This User is called {self.name} {self.last_name}. He/She is {self.age}.')
