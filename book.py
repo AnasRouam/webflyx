3ecdeb5972f07ebfaab2ecee64a99e2646f20a69
AnasRouam <114496477+AnasRouam@users.noreply.github.com>
2025-04-01 09:27:32 +0000

    Added id to books class

--- book.py
+++ book.py
@@ -1,6 +1,7 @@
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, id, title, author, year):
         # year being the year the book was published
+        self.id = id
         self.title = title
         self.author = author
         self.year = year
@@ -9,4 +10,8 @@ class Book:
         print(f'Book("{self.title}", "{self.author}", "{self.year}")')
 
     def __str__(self):
-        print(f'This book is titled {self.title}, authored by {self.author} and published in {self.year}')
+        return f'This book is titled {self.title}, authored by {self.author} and published in {self.year}'
+
+book1 = Book(1, "The fault in our stars", "John Green", 2012)
+print(book1)
+print(book1.__repr__())
